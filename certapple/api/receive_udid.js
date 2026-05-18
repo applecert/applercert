f1641e80bd18131a4b97999df217974ec2576350
applecert <tchy432@gmail.com>
2026-05-18 13:51:52 +0700

    Update receive_udid.js

--- certapple/api/receive_udid.js
+++ certapple/api/receive_udid.js
@@ -8,16 +8,18 @@ export default async function handler(req, res) {
         for await (const chunk of req) {
             body += chunk;
         }
+        
+        // Trích xuất UDID từ dữ liệu Apple gửi lên
         const match = body.match(/<key>UDID<\/key>[\s]*<string>([a-zA-Z0-9\-]+)<\/string>/);
         const udid = match ? match[1] : '';
 
         if (udid) {
-            // Đẩy khách về lại trang chủ kèm UDID
-            res.redirect(301, `https://ipaviet-cert.vercel.app/?udid=${udid}`);
+            // Chuyển hướng về trang chủ web của bạn kèm theo mã UDID
+            res.redirect(301, `https://applercert.vercel.app/?udid=${udid}`);
         } else {
-            res.status(400).send('Lỗi trích xuất UDID.');
+            res.status(400).send('Không trích xuất được UDID. Vui lòng thử lại.');
         }
     } else {
-        res.status(405).send('Chỉ nhận POST.');
+        res.status(405).send('Phương thức không được hỗ trợ.');
     }
 }
