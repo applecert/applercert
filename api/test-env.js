ae572beb9d9b0249f7da8bcb02e6ce98a1f8a913
applecert <tchy432@gmail.com>
2026-07-02 04:30:01 +0700

    diagnose: update test-env to test firebase initialization

--- api/test-env.js
+++ api/test-env.js
@@ -1,10 +1,48 @@
+const admin = require('firebase-admin');
+const { getFirestore } = require('firebase-admin/firestore');
+
 module.exports = async function handler(req, res) {
+  let initError = null;
+  let parseError = null;
+  let parsedAccount = null;
+
+  try {
+    if (process.env.FIREBASE_SERVICE_ACCOUNT) {
+      parsedAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+    }
+  } catch (err) {
+    parseError = { message: err.message, stack: err.stack };
+  }
+
+  if (!admin.getApps().length) {
+    try {
+      if (parsedAccount) {
+        admin.initializeApp({
+          credential: admin.cert(parsedAccount)
+        });
+      } else {
+        throw new Error("No parsed service account available");
+      }
+    } catch (err) {
+      initError = { message: err.message, stack: err.stack };
+    }
+  }
+
+  let dbOk = false;
+  let dbError = null;
+  try {
+    const db = getFirestore();
+    dbOk = !!db;
+  } catch (err) {
+    dbError = { message: err.message, stack: err.stack };
+  }
+
   res.status(200).json({
     hasServiceAccount: !!process.env.FIREBASE_SERVICE_ACCOUNT,
-    serviceAccountLength: process.env.FIREBASE_SERVICE_ACCOUNT ? process.env.FIREBASE_SERVICE_ACCOUNT.length : 0,
-    hasCaptchaSecret: !!process.env.CAPTCHA_SECRET_KEY,
-    captchaSecretLength: process.env.CAPTCHA_SECRET_KEY ? process.env.CAPTCHA_SECRET_KEY.length : 0,
-    cwd: process.cwd(),
-    nodeVersion: process.version
+    parseError,
+    initError,
+    dbOk,
+    dbError,
+    parsedKeys: parsedAccount ? Object.keys(parsedAccount) : null
   });
 };
